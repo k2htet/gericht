8f00a8c06a265bf7e47c0e1a1c3021484de723d2
KoHtet <kokoh6329@gmail.com>
2022-05-17 20:35:48 +0630

    remove loading

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from "react";
-import { useState, useEffect } from "react";
-import { CircularProgress, Box } from "@mui/material";
+
 import {
   AboutUs,
   Chef,
@@ -15,43 +14,18 @@ import {
 import { Navbar } from "./components";
 import "./App.css";
 
-const App = () => {
-  const [loading, setloading] = useState(false);
-
-  useEffect(() => {
-    window.addEventListener("load", () => setloading(true));
-    // setTimeout(() => {
-    //   setloading(true);
-    // }, 5000);
-  }, []);
-
-  return loading ? (
-    <div>
-      <Navbar />
-      <Header />
-      <AboutUs />
-      <SpecialMenu />
-      <Chef />
-      <Intro />
-      <Laurels />
-      <Gallery />
-      <FindUs />
-      <Footer />
-    </div>
-  ) : (
-    <Box
-      sx={{
-        display: "flex",
-        height: "100vh",
-        width: "100vw",
-        justifyContent: "center",
-        background: "black",
-        alignItems: "center",
-      }}
-    >
-      <CircularProgress />
-    </Box>
-  );
-};
-
+const App = () => (
+  <div>
+    <Navbar />
+    <Header />
+    <AboutUs />
+    <SpecialMenu />
+    <Chef />
+    <Intro />
+    <Laurels />
+    <Gallery />
+    <FindUs />
+    <Footer />
+  </div>
+);
 export default App;
