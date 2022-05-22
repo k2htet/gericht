4909e8a91d76a914b2fea719581d22e8823f4677
KoHtet <kokoh6329@gmail.com>
2022-05-22 13:00:17 +0630

    add loading

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-
+import { useState, useEffect } from "react";
 import {
   AboutUs,
   Chef,
@@ -13,19 +13,31 @@ import {
 } from "./container";
 import { Navbar } from "./components";
 import "./App.css";
+import { PuffLoader } from "react-spinners";
 
-const App = () => (
-  <div>
-    <Navbar />
-    <Header />
-    <AboutUs />
-    <SpecialMenu />
-    <Chef />
-    <Intro />
-    <Laurels />
-    <Gallery />
-    <FindUs />
-    <Footer />
-  </div>
-);
+const App = () => {
+  const [loading, setloading] = useState(true);
+
+  useEffect(() => setloading(false), []);
+
+  return loading ? (
+    <div className="loading-container">
+      <PuffLoader color="#DCCA87" loading={loading} size={150} />
+    </div>
+  ) : (
+    <div>
+      <Navbar />
+      <Header />
+      <AboutUs />
+      <SpecialMenu />
+      <Chef />
+      <Intro />
+      <Laurels />
+      <Gallery />
+      <FindUs />
+      <Footer />
+    </div>
+  );
+};
+
 export default App;
